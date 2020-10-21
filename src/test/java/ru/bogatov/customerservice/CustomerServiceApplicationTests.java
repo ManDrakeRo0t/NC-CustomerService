14efff4da5d98a2e059d4c50396bafc2ed64588b
ManDrakeRo0t <danik21.01.01@gmail.com>
2020-10-21 16:13:38 +0400

    init project / connected db

---
+++ src/test/java/ru/bogatov/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bogatov.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
